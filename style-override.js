0297fe92bf4ae84398a7a3ab4778edd9a231cffa
Acfboy <acfboyu@outlook.com>
2020-06-27 07:10:58 +0800

    Add files via upload

--- style-override.js
+++ style-override.js
@@ -6,6 +6,8 @@ const generateOverride = (params = {}) => {
   		.menu-container{background:${params.headcolor};}
   	   }`
    result += `.main .right .id_card{ background: ${params.idcolor};} .main .right .noticecard{background: ${params.noticecolor};}`
+   result += `article.post{ background: ${params.postback} !important;}`
+   result += `.toccard{background: ${params.toccolor}!important;}`
   return result
 }
 
